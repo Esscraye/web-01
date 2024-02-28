ba2e69e4bb964e6415cc3d3788b0c14b0309672a
Esscraye <esscrayebat@netcourrier.com>
2024-02-28 14:30:06 +0100

    feature(): css-loader

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,6 +5,10 @@ import { ScoreComponent } from "./app/scripts/score.js";
 // TODO #import-components: use ES named imports to import WelcomeComponent, GameComponent a ScoreComponent
 // TODO #import-css: use ES side-effect imports to import styles/style.css
 
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import "./app/styles/style.css";
+
+
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
 router
